f2c2d2718254c9bd1462732eb366723ae4a9de19
DarKsama364 <oplisson7@gmail.com>
2024-09-13 09:55:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ class Monde:
     
     def herbeMangee(self, i, j):
         for i,j in range(len(self.carte)):
-            self.carte[i][j] = 0 if self.carte[i][j] >= 30
+            if self.carte[i][j] >= 30:
+                self.carte[i][j] = 0 
     
-    def nbHerbe(self, nbHerbe):
+    def nbHerbe(self):
         nbHerbe = 0
         for i,j in range(len(self.carte)):
             if self.carte[i][j] >= 30:
@@ -24,59 +25,51 @@ class Monde:
     def getCoefCarte(self, i, j):
         return self.carte[i][j]
 
-    
-
 class Mouton:
-    def __init__(self, gain_nourriture, position, taux_reproduction, monde)
-    self.gain_nourriture = 4
-    self.position = (i,j)
-    self.energie = random.randint(1, 2*self.gain_nourriture)
-    self.taux_reproduction = 4
-    self.monde = monde
+    def __init__(self, gain_nourriture, position, energie, taux_reproduction, monde):
+        self.gain_nourriture = 4
+        self.position = (i,j)
+        self.energie = random.randint(1, 2*self.gain_nourriture)
+        self.taux_reproduction = 4
+        self.monde = monde
 
     def variationEnergie(self):
         if self.monde.carte[self.position[0]][self.position[1]] >= self.monde.duree_repousse:
             self.energie += self.gain_nourriture
-            self.monde.carte[self.position[0]][self.position[1]] = 0
+            self.monde.herbeMangee(self.position[0], self.position[1])
         else:
             self.energie = self.energie - 1
 
     def deplacement(self):
-        deplace = random.randint(0,8)
         position = list(self.position)
-        binds = {
-            1: (position[0]-1, position[1]-1),
-            2: position[1] -1,
-            3: (position[0]+1, position[1]-1),
-            4: position[0] + 1,
-            5: (position[0]+1, position[1]+1),
-            6: position[1] + 1,
-            7: (position[0]-1, position[1]+1),
-            8: position[0] - 1
-        }
-        position = binds[deplace]
+        position[0] = position[0] + random.randint(-1, 1)
+        if position[0] < 0:
+            position[0] = 50
+        elif position[0] > dimension:
+            position[0] = 0
+
+        position[1] = position[1] + random.randint(-1, 1)
+        if position[1] < 0:
+            position[1] = 50
+        elif position[1] > dimension:
+            position[1] = 0
+
         self.position = tuple(position)
         return self.position
-    
-    def setPosition(self, pere):
-        chance = randint(0, self.taux_reproduction)
-        if chance == 4:
-            bebe = Mouton(4, pere.position, 50, 4, pere.monde)
-            bebe.position[0], bebe.position[1] = self.position[0], self.position[1]
-        
-    def getPosition(self):
-        return f'La position de mouton --> {self.position}'
+
+    def bebe(self, pere):
+        x = random.randint(0,100)
+        if x <= taux_reproduction:
+            bebe = Mouton(4,pere.position, random.randint(1, 2*self.gain_nourriture), 4, pere.monde)
 
 class Simulation:
-    def __init__(self, nombre_moutons, fin_du_monde, monde):
+    def __init__(self, nombre_moutons, horloge, fin_du_monde, moutons, monde, resultats_herbe, resultats_moutons):
         self.nombre_moutons = nombre_moutons
         self.horloge = 0
-        self.monde = Monde()
-        self.moutons = [Mouton(4, (random.randint(0,50) for i in range(2)), 4, Monde()) for mouton in range(self.nombre_moutons)]
-        self.fin_du_monde = 5000
+        self.fin_du_monde = 10
+        self.moutons = [Mouton(4, (random.randint(0, 50) for i in range(2)) 4, Monde()) for mouton in range(self.nombre_moutons)]
         self.monde = monde
-        self.resultat_herbe = self.monde.carte
-    def
+        self.resultats_herbe = self.monde.nbHerbe()
+        self.resultats_moutons
 
-
-
+    def simMouton(self):
